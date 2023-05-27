7748d3abe4833045e25c3a0f5ac7b4a4091d08cd
sumin <68219918+MinKimS@users.noreply.github.com>
2023-05-28 01:59:33 +0900

    smartphone ui
    핸드폰 스크롤, on/off등의 기능 구현

---
+++ Assets/Scripts/TalkData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class TalkData : MonoBehaviour
+{
+    public TextMeshProUGUI readNumText, talkText, nameText;
+    public GameObject tail;
+    public Image profileImage;
+    public string userName;
+    public int readNum;
+}
