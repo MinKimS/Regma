87bb51996c7e90903886f487d047a78fa42a6049
YEAHWA1010 <109790117+YEAHWA1010@users.noreply.github.com>
2023-05-29 04:17:12 +0900

    mainCh 수정

--- Assets/Scripts/player/playermoving.cs
+++ Assets/Scripts/player/playermoving.cs
@@ -6,21 +6,25 @@ public class playermoving : MonoBehaviour
 {
     public float moveSpeed = 1f;
     public float runSpeed = 7f;
+    public float jumpForce = 20f;
 
     private bool isRunning = false;
+    private bool isJumping = false;
     private Rigidbody2D rb;
 
-    private void Start()
+    void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.gravityScale = 0f; // 중력 비활성화
+        rb.gravityScale = 1f; // 중력 활성화
     }
 
-    private void Update()
+    void Update()
     {
         float moveInputX = Input.GetAxisRaw("Horizontal");
         float moveInputY = Input.GetAxisRaw("Vertical");
-        rb.velocity = new Vector2(moveInputX * GetSpeed(), moveInputY * GetSpeed());
+
+        // 상하 이동
+        rb.velocity = new Vector2(moveInputX * GetSpeed(), rb.velocity.y);
 
         if (Input.GetKey(KeyCode.LeftControl))
         {
@@ -29,6 +33,14 @@ public class playermoving : MonoBehaviour
         else
         {
             isRunning = false;
+        }
+
+        // 점프
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0f); // 수직 속도 초기화
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            isJumping = true;
         }
     }
 
@@ -41,6 +53,14 @@ public class playermoving : MonoBehaviour
         else
         {
             return moveSpeed;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
         }
     }
 }
