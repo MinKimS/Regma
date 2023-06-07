e3d5506a57783410b89ba62168cd2efe728d778b
YEAHWA1010 <109790117+YEAHWA1010@users.noreply.github.com>
2023-06-07 11:47:25 +0900

    3

--- Assets/Scripts/player/Damage.cs
+++ Assets/Scripts/player/Damage.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 
 public class Damage : MonoBehaviour
 {
+    public GameObject ItemHp;
     public Image HpScreen;
     bool isActive = false;
 
@@ -14,16 +15,16 @@ public class Damage : MonoBehaviour
         HpScreen.color = Color.clear;
     }
 
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player") && !isActive)
-        {
-            isActive = true;
-            ShowHpScreen();
-        }
-    }
+    //void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.CompareTag("Player") && collision.CompareTag("Item"))
+    //    {
+    //        isActive = true;
+    //        ShowHpScreen();
+    //    }
+    //}
 
-    void ShowHpScreen()
+    public void ShowHpScreen()
     {
         HpScreen.color = new Color(1f, 1f, 1f, 1f);
     }
